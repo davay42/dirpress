dc74708742bba00d8a25a25a543b8aa8bfe958f7
davay <davay@defucc.me>
2023-12-05 15:16:17 +0700

    page transition

--- components/Layout.vue
+++ components/Layout.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { useData } from 'vitepress'
+import { useData, useRoute } from 'vitepress'
 import { data as meta } from '../data/meta.data.js'
 import { data as pages } from '../data/pages.data.js'
 
@@ -8,6 +8,8 @@ import '../styles/main.postcss'
 import '../styles/transitions.postcss'
 
 const { frontmatter: f, page, params } = useData()
+
+const route = useRoute()
 
 </script>
 
@@ -27,29 +29,30 @@ main
       :aria-current="page.relativePath.includes(main.slug)"
     ) {{ main.title }} 
 
-  img.cover(
-    v-if="f.cover" 
-    :src="`/media/${f.slug}-cover.webp`")
 
-  .flex.flex-wrap.items-center.text-2xl.gap-2.px-2
-    a.p-2.opacity-40(
-      v-if="f.category"
-      :href="`/catalog/${f.category.slug}/`") {{ f.category.title }}
-    .flex.p-2 {{ f.title }}
 
-  transition(name="fade")
-    article.flex-auto(:key="page.route")
+  transition(name="fade" mode="out-in")
+    article.flex-auto(:key="route.path")
+      img.cover(
+        v-if="f.cover" 
+        :src="`/media/${f.slug}-cover.webp`")
+
+      .flex.flex-wrap.items-center.text-2xl.gap-2.px-2
+        a.p-2.opacity-40(
+          v-if="f.category"
+          :href="`/catalog/${f.category.slug}/`") {{ f.category.title }}
+        .flex.p-2 {{ f.title }}
       content.markdown-body
 
-  aside.gallery(v-if="f.gallery")
-    .text-lg Gallery
-    .p-0(v-for="image in f.gallery") {{ image.directus_files_id }}
+      aside.gallery(v-if="f.gallery")
+        .text-lg Gallery
+        .p-0(v-for="image in f.gallery") {{ image.directus_files_id }}
 
-  .flex.flex-col.gap-4.p-4 
-    a.p-4.bg-light-100.shadow-xl.text-2xl.rounded-xl(
-      v-for="page in f.pages" :key="page"
-      :href="'/' + [page?.root?.root?.slug, page?.root?.slug, page.slug].filter(Boolean).join('/') + '/'"
-      ) {{ page.title }}
+      .flex.flex-wrap.gap-4.p-4 
+        a.p-4.bg-light-100.shadow-xl.text-2xl.rounded-xl(
+          v-for="page in f.pages" :key="page"
+          :href="'/' + [page?.root?.root?.slug, page?.root?.slug, page.slug].filter(Boolean).join('/') + '/'"
+          ) {{ page.title }}
 
   footer ©️ {{ meta.company }} 
 
